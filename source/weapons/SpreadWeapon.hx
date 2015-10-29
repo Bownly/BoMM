079011bfda3b058ed1c3c6a935c1a3d5d9491505
Darnsworth <mario5@mail.usf.edu>
2015-10-28 22:01:32 -0400

    Cluster, Spread, Plus, and X weapons added

---
+++ source/weapons/SpreadWeapon.hx
@@ -0,0 +1,53 @@
+package weapons;
+import flixel.FlxObject;
+import flixel.group.FlxTypedGroup;
+
+/**
+ * ...
+ * @author me
+ */
+class SpreadWeapon extends WeaponTemplate
+{
+
+
+	public function new(Name:String, ?Bullets:FlxTypedGroup<Bullet>) 
+	{
+		super(Name, Bullets);
+		
+		damage = 5;
+		juice = 9;
+		juiceMax = 9;
+		juiceCost = 1;
+	}
+	
+	public override function shoot(_player:Player)
+	{
+		if (_player.bulletArray.countLiving() < maxBullets && juice >= juiceCost) 
+		{
+			_player.postShotTimer = .33;
+			
+			if (_player.flipX)
+			{	
+				var newBullet = new Bullet(_player.x - 8, _player.y + 8, 200, FlxObject.LEFT, _player.damage + damage, 80);
+				var newBullet1 = new Bullet(_player.x + 2, _player.y + 8, 200, FlxObject.LEFT + FlxObject.CEILING + FlxObject.LEFT, _player.damage + damage, 80);
+				var newBullet2 = new Bullet(_player.x + 2, _player.y + 8, 200, FlxObject.LEFT + FlxObject.FLOOR + FlxObject.LEFT, _player.damage + damage, 80);
+				
+				_player.bulletArray.add(newBullet);
+				_player.bulletArray.add(newBullet1);
+				_player.bulletArray.add(newBullet2);
+			}
+			else
+			{
+				var newBullet = new Bullet(_player.x + 8, _player.y + 8, 200, FlxObject.RIGHT, _player.damage + damage, 80);
+				var newBullet1 = new Bullet(_player.x + 2, _player.y + 8, 200, FlxObject.RIGHT + FlxObject.CEILING + FlxObject.RIGHT, _player.damage + damage, 80);
+				var newBullet2 = new Bullet(_player.x + 2, _player.y + 8, 200, FlxObject.RIGHT + FlxObject.FLOOR + FlxObject.RIGHT, _player.damage + damage, 80);
+				
+				_player.bulletArray.add(newBullet);
+				_player.bulletArray.add(newBullet1);
+				_player.bulletArray.add(newBullet2);
+				
+			}
+			juice -= juiceCost;
+		}
+	}
+}
